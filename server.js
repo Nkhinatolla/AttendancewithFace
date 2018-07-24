618f40054b48c8628e32671703a9e9ac8ae08d0c
Nurbergen <nhinatolla@gmail.com>
2018-07-24 14:16:43 +0600

    Nurik

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const express = require('express');
 const JSONStream = require('JSONStream');
 const bodyParser = require('body-parser');
 const app = express();
+const axios = require ('axios');
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.post('/kotak', function(req, res) {
-   return res.json({data: "huy"})
+  var parent = "A1"
+  var from = 0
+  axios.get("http://post.city.kz/objects/{parent}?from={from}")
+  .then(response => {
+    console.log(response.data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+   return res.json(response.data)
   console.log('lol');
 })
 
